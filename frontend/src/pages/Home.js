70aef5625122c6ccd2e6e0c20e9c6a11f09011bb
lwioSoton <lwi1g19@soton.ac.uk>
2025-04-24 14:32:46 +0100

    EDIT: Changed Home page text

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,14 +4,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Joy from './Joy';
 
 function Home() {
+  //Wording follows ISO 9241-110:2020 – Ergonomics of human-system interaction
+  //https://www.gov.uk/service-manual/design/writing-for-user-interfaces
   //TODO: Research for what wording to use to make it more palatable to stressed people
-  //TODO: 
+  // Acknowledge the emotion (without being dramatic)
+  // Don’t use metaphors or idioms (can be misinterpreted)
+  // Be clear and direct (no euphemisms)  
+  // Avoid passive voice — use active verbs so users know what to do next
+  // Minimize steps — simplify interactions
+  
   return (
     <div>
       <h1 className="home-title">Welcome to the Lloyds Banking Group Financial Helppage</h1>
       <div className="home-container">
         <h1 className="home-title">Financial Help</h1>
-        <h1 className="home-text">Lloyds has dedicated support for customers facing financial difficulties or money worries. They encourage customers to get in touch sooner rather than later.</h1>
+        <h1 className="home-text">Many people face financial stress, but you are not alone. We have dedicated support for customers facing financial difficulties or money worries. Users in any stage of financial stress are encouraged to use them.</h1>
       </div>
       <div className="home-container">
         <h1 className="home-title">Mental Help</h1>
