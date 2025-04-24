1e25bd345d6c23d9a9422a19a340a60fd263d835
lwioSoton <lwi1g19@soton.ac.uk>
2025-04-24 11:47:41 +0100

    EDIT: Added boxes for information

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,11 +3,19 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="home-container">
-      <h1>Welcome to the Lloyds Banking Group Dashboard</h1>
-      <p>Navigate through the menu to explore features.</p>
+    <div>
+      <h1 className="home-title">Welcome to the Lloyds Banking Group Financial Helppage</h1>
+      <div className="home-container">
+      <h1 className="home-title">Financial Help</h1>  
+      </div>
+      <div className="home-container">
+        <h1 className="home-title">Mental Help</h1>
+      </div>
+      <div className="home-container">
+        <h1 className="home-title">Talk to Joy</h1>
+      </div>
     </div>
-  );
+    );
 }
 
 export default Home;
